28cfbd2770c0cf13916a60c695147c6642c303f1
den4ik2975 <den41k2975@yandex.ru>
2025-02-16 22:32:05 +0300

    feat: nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  ssr: false,
+
+  app: {
+    head: {
+      title: 'Full Stack App'
+    }
+  },
+
+  compatibilityDate: '2025-02-16'
 })
